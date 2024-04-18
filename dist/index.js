7e310b80fa7f59b956890e0fa462680cd116c160
Akash Deep Singhal <akigupta131@gmail.com>
2024-04-18 17:56:40 +0530

    export default app error resolved

--- dist/index.js
+++ dist/index.js
@@ -6,13 +6,14 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
 const port = process.env.PORT || 8080;
-app.get('/', (_req, res) => {
-    return res.send('Express Typescript on Vercel');
+app.get("/", (_req, res) => {
+    return res.send("Express Typescript on Vercel");
 });
-app.get('/ping', (_req, res) => {
-    return res.send('pong 🏓');
+app.get("/ping", (_req, res) => {
+    return res.send("pong 🏓");
 });
 app.listen(port, () => {
     return console.log(`Server is listening on ${port}`);
 });
+exports.default = app;
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,18 @@
-import express, { Request, Response } from 'express'
+import express, { Request, Response } from "express";
 
-const app = express()
-const port = process.env.PORT || 8080
+const app = express();
+const port = process.env.PORT || 8080;
 
-app.get('/', (_req: Request, res: Response) => {
-	return res.send('Express Typescript on Vercel')
-})
+app.get("/", (_req: Request, res: Response) => {
+  return res.send("Express Typescript on Vercel");
+});
 
-app.get('/ping', (_req: Request, res: Response) => {
-	return res.send('pong 🏓')
-})
+app.get("/ping", (_req: Request, res: Response) => {
+  return res.send("pong 🏓");
+});
 
 app.listen(port, () => {
-	return console.log(`Server is listening on ${port}`)
-})
+  return console.log(`Server is listening on ${port}`);
+});
+
+export default app;
